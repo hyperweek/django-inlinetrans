6516dcc9c65ea179943b15912ecccbc21f816de7
Daniel Nordberg <dnordberg@gmail.com>
2009-10-01 16:22:00 +0200

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-inlinetrans',
+    version='0.1.0',
+    description='Django-inlinetrans is a django application that allows the translation of django templates from the rendered html in the browser.',
+    author='dnordberg',
+    author_email='dnordberg@gmail.com',
+    url='http://github.com/dnordberg/django-inlinetrans/tree/master',
+    packages=find_packages(),
+    include_package_data = True,
+)
